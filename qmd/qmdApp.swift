804a2bbcab63a8a17deb7327ad26444adec4e46e
Kentaro Kawano <kentaro.kawano@gmail.com>
2024-03-12 21:53:11 +0900

    Initial Commit

---
+++ qmd/qmdApp.swift
@@ -0,0 +1,17 @@
+//
+//  qmdApp.swift
+//  qmd
+//
+//  Created by 河野健太朗 on 2024/03/12.
+//
+
+import SwiftUI
+
+@main
+struct qmdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
