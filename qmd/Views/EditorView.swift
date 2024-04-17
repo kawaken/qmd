e216851940b949246b6fbc47d69a55cd2d0dbd2b
Kentaro Kawano <kentaro.kawano@gmail.com>
2024-04-17 15:46:15 +0900

    ウィンドウのボタンを無効化

--- qmd/Views/EditorView.swift
+++ qmd/Views/EditorView.swift
@@ -11,9 +11,30 @@ struct EditorView: View {
     @State var text: String = ""
     
     var body: some View {
+        HostingWindowFinder { window in
+            guard let w = window else { return }
+            w.standardWindowButton(.closeButton)?.isEnabled = false
+            w.standardWindowButton(.miniaturizeButton)?.isEnabled = false
+            w.standardWindowButton(.zoomButton)?.isEnabled = false
+        }
         TextEditor(text: $text)
             .padding(10)
             .background(.white)
+    }
+}
+
+struct HostingWindowFinder: NSViewRepresentable {
+    var callback: (NSWindow?) -> ()
+    
+    func makeNSView(context: Self.Context) -> NSView {
+        let view = NSView()
+        DispatchQueue.main.async { [weak view] in
+            self.callback(view?.window)
+        }
+        return view
+    }
+    
+    func updateNSView(_ nsView: NSView, context: Context) {
     }
 }
 
