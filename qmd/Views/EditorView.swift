c5eb3c281e84df518965f4b3e74cb5c3b12e5b98
Kentaro Kawano <kentaro.kawano@gmail.com>
2024-04-22 00:16:59 +0900

    ショートカットでコピペの実装ができた

--- qmd/Views/EditorView.swift
+++ qmd/Views/EditorView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct EditorView: View {
     @State var text: String = ""
     
+    private let pasteboard = NSPasteboard.general
+    
     var body: some View {
         HostingWindowFinder { window in
             guard let w = window else { return }
@@ -18,14 +20,18 @@ struct EditorView: View {
             w.standardWindowButton(.zoomButton)?.isEnabled = false
         }.frame(width:0,height:0) // disable frame
         
-        HStack {
-            TextEditor(text: $text)
-                .padding(10)
-                .background(.white)
-        }
-            .background(KeyEventHandling(modifierFlags: .command, keyUpCharacter: "a") {
-                print(text)
-            })
+        
+        TextEditor(text: $text)
+            .padding(10)
+            .background(.white)
+            .onKeyPress { keyPress in
+                if (keyPress.key == .return && keyPress.modifiers == .command) {
+                    pasteboard.declareTypes([.string], owner: nil)
+                    pasteboard.setString(text, forType: .string)
+                    return .handled
+                }
+                return .ignored
+            }
     }
 }
 
@@ -40,60 +46,6 @@ struct HostingWindowFinder: NSViewRepresentable {
         return view
     }
     
-    func updateNSView(_ nsView: NSView, context: Context) {
-    }
-}
-
-struct KeyEventHandling: NSViewRepresentable {
-    
-    var handler:Handler
-    
-    init(modifierFlags: NSEvent.ModifierFlags, keyUpCharacter: String?, action: @escaping () -> ()) {
-        self.handler = Handler(modifierFlags: modifierFlags, keyUpCharacter:keyUpCharacter, action:action)
-    }
-
-    class Handler {
-        var modifierFlags: NSEvent.ModifierFlags = []
-        var keyUpCharacter: String? = nil
-        var action: () -> ()
-        
-        init(modifierFlags: NSEvent.ModifierFlags, keyUpCharacter: String?, action: @escaping () -> ()) {
-            self.modifierFlags = modifierFlags
-            self.keyUpCharacter = keyUpCharacter
-            self.action = action
-        }
-        
-        func handleEvent(_ event: NSEvent) {
-            print("hello")
-            if (event.modifierFlags.contains(modifierFlags) && event.characters == keyUpCharacter) {
-                self.action()
-            }
-        }
-    }
-    
-    class KeyView: NSView {
-        var handler: Handler?
-        
-        override var acceptsFirstResponder: Bool {
-            true
-        }
-                
-        override func keyUp(with event: NSEvent) {
-            super.keyUp(with: event)
-            handler?.handleEvent(event)
-        }
-    }
-    
-    func makeNSView(context: Context) -> NSView {
-        let view = KeyView()
-        view.handler = handler
-        
-        DispatchQueue.main.async { // wait till next event cycle
-            view.window?.makeFirstResponder(view)
-        }
-        return view
-    }
-
     func updateNSView(_ nsView: NSView, context: Context) {
     }
 }
