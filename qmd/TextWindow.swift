8df5140828c0fc35662926fce14ad8862021134a
Kentaro Kawano <kentaro.kawano@gmail.com>
2024-05-09 18:15:07 +0900

    Windowのスタイルを変更

--- qmd/TextWindow.swift
+++ qmd/TextWindow.swift
@@ -6,8 +6,10 @@
 //
 
 import Cocoa
+import SwiftUI
 
 class TextWindow: NSWindow {
+    
     init() {
         // ウィンドウのサイズ
         let windowWidth: CGFloat = 400
@@ -15,8 +17,20 @@ class TextWindow: NSWindow {
         
         // ウィンドウのcontentRectを設定
         let contentRect = NSRect(x: 0, y: 0, width: windowWidth, height: windowHeight)
+        let styleMask: NSWindow.StyleMask = [
+            .titled,
+            .closable,
+            .resizable,
+            .fullSizeContentView
+        ]
         
-        super.init(contentRect: contentRect, styleMask: [.borderless], backing: .buffered, defer: false)
+        super.init(contentRect: contentRect, styleMask: styleMask, backing: .buffered, defer: false)
+        
+        self.titlebarAppearsTransparent = true
+        self.titleVisibility = .hidden
+        
+        self.standardWindowButton(.miniaturizeButton)?.isHidden = true
+        self.standardWindowButton(.zoomButton)?.isHidden = true
         
         self.level = .floating
         self.isReleasedWhenClosed = false
